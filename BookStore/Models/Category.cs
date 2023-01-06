7472e72c5d8baabe865dd12567872e1c0c19ccde
Melissa Astbury <melissa.astbury@footasylum.com>
2023-01-06 09:54:10 +0000

    Added new model of Category which will later be used to create a DB

---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace BookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
