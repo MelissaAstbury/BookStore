a79df97d273da860cfcc52b62b5d22f6c2fd4293
Melissa Astbury <melissa.astbury@footasylum.com>
2023-01-06 16:12:04 +0000

    Added first data model with data annotations reading for the DB migration

--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Models
 {
     public class Category
     {
+        [Key] // Primary Key
         public int Id { get; set; }
 
+        [Required] // Has to be provided 
         public string Name { get; set; }
 
         public int DisplayOrder { get; set; }
